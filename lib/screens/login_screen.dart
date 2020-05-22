6f7b7ea397aead0f5ceb95fc6894de07a0611d5f
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2020-05-22 02:12:05 -0300

    espaçamento

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -41,70 +41,71 @@ class _LoginScreenState extends State<LoginScreen> {
           alignment: Alignment.center,
           margin: const EdgeInsets.all(32),
           child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            elevation: 16,
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Observer(builder: (_) {
+                    return CustomTextField(
+                      hint: 'E-mail',
+                      prefix: Icon(Icons.account_circle),
+                      textInputType: TextInputType.emailAddress,
+                      onChanged: loginStore.setEmail,
+                      enabled: !loginStore.loading,
+                    );
+                  }),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Observer(builder: (_) {
+                    return CustomTextField(
+                      hint: 'Senha',
+                      prefix: Icon(Icons.lock),
+                      obscure: loginStore.visible,
+                      onChanged: loginStore.setPassword,
+                      enabled: !loginStore.loading,
+                      suffix: CustomIconButton(
+                        radius: 32,
+                        iconData: loginStore.visible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                        onTap: loginStore.setVisible,
+                      ),
+                    );
+                  }),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Observer(builder: (_) {
+                    return SizedBox(
+                      height: 44,
+                      child: RaisedButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(32),
+                        ),
+                        child: loginStore.loading
+                            ? CircularProgressIndicator(
+                                valueColor: AlwaysStoppedAnimation<Color>(
+                                    Colors.white),
+                              )
+                            : Text('Login'),
+                        color: Theme.of(context).primaryColor,
+                        disabledColor:
+                            Theme.of(context).primaryColor.withAlpha(100),
+                        textColor: Colors.white,
+                        onPressed: loginStore.loginPressed,                          
+                      ),
+                    );
+                  })
+                ],
               ),
-              elevation: 16,
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Observer(builder: (_) {
-                      return CustomTextField(
-                        hint: 'E-mail',
-                        prefix: Icon(Icons.account_circle),
-                        textInputType: TextInputType.emailAddress,
-                        onChanged: loginStore.setEmail,
-                        enabled: !loginStore.loading,
-                      );
-                    }),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    Observer(builder: (_) {
-                      return CustomTextField(
-                        hint: 'Senha',
-                        prefix: Icon(Icons.lock),
-                        obscure: loginStore.visible,
-                        onChanged: loginStore.setPassword,
-                        enabled: !loginStore.loading,
-                        suffix: CustomIconButton(
-                          radius: 32,
-                          iconData: loginStore.visible
-                              ? Icons.visibility
-                              : Icons.visibility_off,
-                          onTap: loginStore.setVisible,
-                        ),
-                      );
-                    }),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    Observer(builder: (_) {
-                      return SizedBox(
-                        height: 44,
-                        child: RaisedButton(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(32),
-                          ),
-                          child: loginStore.loading
-                              ? CircularProgressIndicator(
-                                  valueColor: AlwaysStoppedAnimation<Color>(
-                                      Colors.white),
-                                )
-                              : Text('Login'),
-                          color: Theme.of(context).primaryColor,
-                          disabledColor:
-                              Theme.of(context).primaryColor.withAlpha(100),
-                          textColor: Colors.white,
-                          onPressed: loginStore.loginPressed,                          
-                        ),
-                      );
-                    })
-                  ],
-                ),
-              )),
+            )
+          ),
         ),
       ),
     );
